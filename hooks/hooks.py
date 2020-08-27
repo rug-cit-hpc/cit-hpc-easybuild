87c46a81d2cfcf1f57f90a3ea7130734b3b864ea
Bob <b.e.droge@rug.nl>
2020-08-27 11:27:20 +0200

    remove --without-verbs, seems to work again...

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -4,4 +4,4 @@ def pre_configure_hook(self, *args, **kwargs):
         self.log.info("[pre-configure hook] Adding SLURM and pmix support")
         #self.cfg['preconfigopts'] = 'sed -i "s/pmix_ext_install_dir\/lib/pmix_ext_install_dir\/lib64/g" ./configure && '
         #self.cfg['configopts'] = self.cfg['configopts'] + '--disable-dlopen --with-slurm --with-pmi=/usr --with-pmi-libdir=/usr --with-pmix=/usr --with-libevent=/usr'
-        self.cfg['configopts'] = self.cfg['configopts'] + '--with-slurm --with-pmi=/usr --with-pmi-libdir=/usr/lib64 --with-pmix=/usr --with-libevent=/usr --without-verbs'
+        self.cfg['configopts'] = self.cfg['configopts'] + '--with-slurm --with-pmi=/usr --with-pmi-libdir=/usr/lib64 --with-pmix=/usr --with-libevent=/usr'
