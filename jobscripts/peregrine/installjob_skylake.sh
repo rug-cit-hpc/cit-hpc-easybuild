46cd4d3baab3cca0a9e20332b7d9e2338a55af3e
Fokke Dijkstra <f.dijkstra@rug.nl>
2022-03-30 11:07:39 +0200

    Changed resource request for CPUs and memory to occupy the whole GPU node.

--- jobscripts/peregrine/installjob_skylake.sh
+++ jobscripts/peregrine/installjob_skylake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=4
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=120G
 #SBATCH --time=12:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
