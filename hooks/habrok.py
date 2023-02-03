d58a4c6d78f09af1d5637a61bc08d21abd66c033
Bob Dröge <b.e.droge@rug.nl>
2023-02-03 12:04:32 +0100

    disable two more pytorch tests

--- hooks/habrok.py
+++ hooks/habrok.py
@@ -5,6 +5,14 @@ def pre_configure_hook(self, *args, **kwargs):
         self.log.info("[pre-configure hook] Making sure that srun is used instead of mpirun")
         self.cfg['wien_mpirun'] = 'srun -n_NP_ _EXEC_'
 
+    # PyTorch: disable two additional tests that fail due to a timeout
+    if self.name == 'PyTorch':
+        self.log.info("[pre-configure hook] Disabling two additional tests...")
+        self.cfg.update('excluded_tests',
+            {'': self.cfg['excluded_tests'][''] + ['distributed/rpc/test_faulty_agent', 'distributed/rpc/test_tensorpipe_agent']}
+        )
+        self.log.info("[pre-configure hook] Updated list of excluded tests: " + ', '.join(self.cfg['excluded_tests']['']))
+
 
 def pre_module_hook(self, *args, **kwargs):
 
