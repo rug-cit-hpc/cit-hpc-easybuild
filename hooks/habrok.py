07682ac936e191963472dfe53a6e3271e9efd608
Bob Dröge <b.e.droge@rug.nl>
2023-06-27 09:39:51 +0200

    disable unified mem for Alpha/ColabFold on V100

--- hooks/habrok.py
+++ hooks/habrok.py
@@ -107,9 +107,13 @@ def pre_module_hook(self, *args, **kwargs):
     # AlphaFold: set data directory, disable unified memory (doesn't work with V100)
     if self.name == 'AlphaFold':
         data_dir = '/scratch/public/%s/%s' % (self.name, self.version)
-        extramodvars = {'ALPHAFOLD_DATA_DIR': data_dir} #, 'TF_FORCE_UNIFIED_MEMORY': '0'}
+        extramodvars = {'ALPHAFOLD_DATA_DIR': data_dir}
         self.log.info('[pre-module hook] Setting ALPHAFOLD_DATA_DIR to ' + data_dir)
         #self.log.info('[pre-module hook] Setting TF_FORCE_UNIFIED_MEMORY to 0')
+        if os.getenv('SW_STACK_ARCH', '') == 'x86_64/intel/skylake_avx512'
+            # Building for V100 without CUDA unified memory support
+            extramodvars['TF_FORCE_UNIFIED_MEMORY'] = 0
+            self.log.info('[pre-module hook] Setting TF_FORCE_UNIFIED_MEMORY to 0')
         self.cfg.update('modextravars', extramodvars)
 
     # ColabFold: set data directory
@@ -117,7 +121,10 @@ def pre_module_hook(self, *args, **kwargs):
         data_dir = '/scratch/public/%s/%s' % (self.name, self.version)
         extramodvars = {'COLABFOLD_DATA_DIR': data_dir} #, 'TF_FORCE_UNIFIED_MEMORY': '0'}
         self.log.info('[pre-module hook] Setting COLABFOLD_DATA_DIR to ' + data_dir)
-        #self.log.info('[pre-module hook] Setting TF_FORCE_UNIFIED_MEMORY to 0')
+        if os.getenv('SW_STACK_ARCH', '') == 'x86_64/intel/skylake_avx512'
+            # Building for V100 without CUDA unified memory support
+            extramodvars['TF_FORCE_UNIFIED_MEMORY'] = 0
+            self.log.info('[pre-module hook] Setting TF_FORCE_UNIFIED_MEMORY to 0')
         self.cfg.update('modextravars', extramodvars)
 
     # AMS: use srun as launcher
