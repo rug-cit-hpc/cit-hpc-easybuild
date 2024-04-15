e08868323264ef3586cc04bed64d08b4e6defd75
Bob Dröge <b.e.droge@rug.nl>
2024-04-15 14:33:33 +0200

    script for updating the base cache

---
+++ scripts/update_lmod_cache_cvmfs_base.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source $(dirname $(realpath $0))/stack.cfg
+
+CONTAINER="docker://ghcr.io/rug-cit-hpc/build-node:${os}"
+
+# Generate base module cache
+DOT_LMOD="${stack_base_std}/.lmod"
+LMOD_RC="${stack_base_std}/.lmod/lmodrc.lua"
+
+apptainer shell -B /cvmfs ${CONTAINER} <<EOF
+modulefiles=\$(find ${stack_base_std}/modules -name "*.lua" | grep -v StdEnv)
+for modulefile in \${modulefiles}
+do
+  echo \${modulefile}
+  # Make a copy of the module files to restore later
+  cp -p \${modulefile} \${modulefile}.bak
+  # Remove MODULEPATH changes
+  sed -i '/MODULEPATH/d' \${modulefile}
+done
+
+if [ ! -d "${DOT_LMOD}" ]
+then
+  mkdir -p "${DOT_LMOD}/cache"
+fi
+
+if [ ! -f "${LMOD_RC}" ]
+then
+  cat > "${LMOD_RC}" <<LMODRCEOF
+propT = {
+}
+scDescriptT = {
+    {
+        ["dir"] = "${DOT_LMOD}/cache",
+        ["timestamp"] = "${DOT_LMOD}/cache/timestamp",
+    },
+}
+LMODRCEOF
+fi
+
+ls ${stack_base_std}/modules
+MODULEPATH=${stack_base_std}/modules
+echo "Using MODULEPATH: \${MODULEPATH}"
+/usr/share/lmod/lmod/libexec/update_lmod_system_cache_files -d ${DOT_LMOD}/cache -t ${DOT_LMOD}/cache/timestamp \$MODULEPATH
+
+# Restore old module files with MODULEPATH setting
+
+for modulefile in \${modulefiles}
+do
+  mv \${modulefile}.bak \${modulefile}
+done
+
+EOF
